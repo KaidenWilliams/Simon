558ac45c13f0830a61484b1f4fd3505d563ec572
KaidenWilliams <kman16409@gmail.com>
2023-03-06 23:33:10 -0700

    Forgot to include sounds which broke everything

--- Login.js
+++ Login.js
@@ -1,8 +1,8 @@
 function login() {
     //Takes user input from text input box with id name, stores it as variable nameUser
-    const nameUser = document.querySelector("#name");
+    const nameEl = document.querySelector("#name");
     //local storage object allows developers to store key-value pairs in web browser, setItem makes item
-    localStorage.setItem("userName", nameUser.value);
+    localStorage.setItem("userName", nameEl.value);
     //Changes current URL to that of Play.html
     window.location.href = "Play.html";
 }
